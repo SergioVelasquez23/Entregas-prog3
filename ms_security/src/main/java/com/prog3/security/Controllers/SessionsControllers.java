38a04fa2126e46e583a1bb38d43226db777c08a5
xnovumt <xnovum17@gmail.com>
2025-04-01 00:29:07 -0500

    f111

---
+++ ms_security/src/main/java/com/prog3/security/Controllers/SessionsControllers.java
@@ -0,0 +1,84 @@
+package com.prog3.security.Controllers;
+
+import com.prog3.security.Models.Session;
+import com.prog3.security.Models.User;
+import com.prog3.security.Repositories.SessionRepository;
+import com.prog3.security.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/sessions")
+public class SessionsControllers {
+    @Autowired
+    SessionRepository theSessionRepository;
+    @Autowired
+    UserRepository theUserRepository;
+
+    @GetMapping("")
+    public List<Session> find(){
+        return this.theSessionRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Session findById(@PathVariable String id) {
+        return this.theSessionRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Session create(@RequestBody Session newSession){
+        return this.theSessionRepository.save(newSession);
+    }
+
+    @PutMapping("/{id}")
+    public Session update(@PathVariable String id, @RequestBody Session newSession){
+        Session actualSession = this.theSessionRepository.findById(id).orElse(null);
+        if (actualSession != null){
+            actualSession.setToken(newSession.getToken());
+            actualSession.setUser(newSession.getUser());
+            this.theSessionRepository.save(actualSession);
+            return actualSession;
+        }else{
+            return null;
+        }
+    }
+
+    @DeleteMapping({"/{id}"})
+    public void delete(@PathVariable String id){
+        this.theSessionRepository.findById(id).ifPresent(theSession -> this.theSessionRepository.delete(theSession));
+    }
+
+    @PostMapping("/{sessionId}/user/{userId}")
+    public String matchUser(@PathVariable String userId, @PathVariable String sessionId) {
+        Session theSession = this.theSessionRepository.findById(sessionId).orElse(null);
+        User theUser = this.theUserRepository.findById(userId).orElse(null);
+
+        if (theSession != null && theUser != null) {
+            theSession.setUser(theUser);
+            this.theSessionRepository.save(theSession);
+            return "User matched to session";
+        } else {
+            return "User or session not found";
+
+        }
+    }
+    
+    @GetMapping("/user/{userId}")
+    public List<Session> getSessionByUser(@PathVariable String userId){
+        return this.theSessionRepository.getSessionByUser(userId);
+    }
+
+    @GetMapping("/timesErrorValidationCode/{userId}")
+    public int getTimesErrorValidationCode(@PathVariable String userId){
+        AtomicInteger timesError = new AtomicInteger();
+        List<Session> theSessions = theSessionRepository.getSessionByUser(userId);
+        theSessions.forEach(session -> {
+            timesError.set(session.getTimesErrorValidationCode());
+        });
+        return timesError.get();
+    }
+}
---
+++ ms_security/src/main/java/com/prog3/security/Models/Role.java
@@ -0,0 +1,24 @@
+package com.prog3.security.Models;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Setter
+@Getter
+@Data
+@Document
+public class Role {
+    @Id
+    private String _id;
+    private String name;
+    private String description;
+
+    public Role(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+}
